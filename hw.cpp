00c383a4e93c4f401af33c24d89d55a2d0704e17
dmnik08 <159506191+dmnik08@users.noreply.github.com>
2025-08-19 15:50:34 -0600

    Add files via upload
    arreglos

--- hw.cpp
+++ hw.cpp
@@ -2,7 +2,13 @@
 
 int main() {
     std::cout << "hello world!" << std::endl;
-    return 0;
+    
+    int array[] = {10,20,30,40,50};
+    int sum= 0;
+    for (int i = 0; i<5; i++){
+             sum+= array[i];
+    }
+    std::cout << "Array sum: " << sum << std::endl;
 }	
 
 
