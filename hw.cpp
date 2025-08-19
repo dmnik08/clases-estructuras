e0bc30b52dfba48a1335ef84be6de6729e3025c9
dmnik08 <159506191+dmnik08@users.noreply.github.com>
2025-08-19 16:00:42 -0600

    Add files via upload
    random

--- hw.cpp
+++ hw.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+#include <cstdlib>
 
 int main() {
     std::cout << "hello world!" << std::endl;
     
-    int array[] = {10,20,30,40,50};
+    int array[10];
     int sum= 0;
-    for (int i = 0; i<5; i++){
+    for (int i = 0; i<10; i++){
+             array[i] = std ::rand ();
              sum+= array[i];
+
     }
+    for (int i =0; i<10; i++)
+            std::cout<< "[" << i << "]:" << array[i] << std::endl;
     std::cout << "Array sum: " << sum << std::endl;
 }	
 
